6dea0dcf7d61e94699b9a88c380999c723e51467
jinyang <jinyang@cs.nyu.edu>
2017-03-28 18:15:40 -0400

    partition matrix

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all : blastest mkltest 
 
-blastest:
-	gcc blastest.c -o blastest -lrt -lgslcblas
+blastest: blastest.c
+	gcc $^ -o blastest -lrt -lgslcblas
 
-mkltest:	
-	 gcc -m64 -static mkltest.c -o mkltest -I/opt/intel/mkl/include -Wl,--start-group -L/opt/intel/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_core  -lmkl_intel_thread -Wl,--end-group -L/opt/intel/compilers_and_libraries/linux/lib/intel64 -liomp5 -ldl -lm -lpthread
+mkltest: mkltest.c	
+	 gcc -m64 -static $^ -o mkltest -I/opt/intel/mkl/include -Wl,--start-group -L/opt/intel/mkl/lib/intel64 -lmkl_intel_lp64 -lmkl_core  -lmkl_intel_thread -Wl,--end-group -L/opt/intel/compilers_and_libraries/linux/lib/intel64 -liomp5 -ldl -lm -lpthread
--- mkltest.c
+++ mkltest.c
@@ -8,6 +8,8 @@
 #include <strings.h>
 #include "mkl.h"
 
+#define MAX_SHARDS 16
+
 void
 init_rand_matrix(float *mat, int size)
 {
@@ -16,39 +18,71 @@ init_rand_matrix(float *mat, int size)
 	}
 }
 
+void
+row_partition(int n)
+{
+    float *matB = (float *)mkl_malloc( n*n*sizeof( float), 64 );
+    assert(matB);
+    init_rand_matrix(matB, n*n);
+
+    double durations[MAX_SHARDS+1];
+    //each shard of computation calculates
+    // C = A * B
+    // where A is of dimension (n/i)*n and B is of dimension n*n
+    for (int i = 1; i <= MAX_SHARDS; i=2*i) {
+	    int sn = n/i;
+	    //allocate memory for matrices aligned on 64-byte boundary
+	    float *matA = (float *)mkl_malloc( sizeof(float)*sn*n, 64 );
+	    float *matC = (float *)mkl_malloc( sizeof( float)*sn*n, 64 );
+	    assert(matA && matC);
+	    init_rand_matrix(matA, sn*n);
+	    bzero(matC, sizeof(float)*sn*n);
+
+	    /* Compute C = A B */
+    	    struct timespec start, end;
+	    assert(clock_gettime( CLOCK_REALTIME, &start)==0);
+	    cblas_sgemm (CblasRowMajor, 
+			    CblasNoTrans, CblasNoTrans, sn, n, n,
+			    1.0, matA, n, matB, n, 0.0, matC, n);
+	    assert(clock_gettime( CLOCK_REALTIME, &end)==0);
+	    durations[i] = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_nsec-start.tv_nsec)/1000000.0;
+
+	    mkl_free(matA);
+	    mkl_free(matC);
+    }
+
+    mkl_free(matB);
+
+    for (int i = 1; i <= MAX_SHARDS; i=i*2) {
+	    printf("%d\t", i);
+    }
+    printf("\n");
+    for (int i = 1; i <= MAX_SHARDS; i=i*2) {
+	    printf("%.1f\t", durations[i]);
+    }
+    printf("\n");
+    for (int i = 1; i <= MAX_SHARDS; i=i*2) {
+	    printf("%.1f\t", durations[1]/durations[i]);
+    }
+    printf("\n");
+}
+
+void
+column_partition(int n)
+{
+
+}
 
 int main(int argc, char **argv)
 {
-    int n = 1000;
+    int n = 1024*8;
     if (argc > 1) {
 	n = atoi(argv[1]);
     }
     printf("MKL matrix multiply two %d x %d matrixes\n", n, n);
-
-    //allocate memory for matrices aligned on 64-byte boundary
-    float *matA = (float *)mkl_malloc( n*n*sizeof( float), 64 );
-    float *matB = (float *)mkl_malloc( n*n*sizeof( float), 64 );
-    float *matC = (float *)mkl_malloc( n*n*sizeof( float), 64 );
-    assert(matA && matB && matC);
-
-    init_rand_matrix(matA, n*n);
-    init_rand_matrix(matB, n*n);
-    bzero(matC, sizeof(float)*n*n);
-
-    /* Compute C = A B */
-    struct timespec start, end;
-    assert(clock_gettime( CLOCK_REALTIME, &start)==0);
-    cblas_sgemm (CblasRowMajor, 
-		    CblasNoTrans, CblasNoTrans, n, n, n,
-		    1.0, matA, n, matB, n, 0.0, matC, n);
-    assert(clock_gettime( CLOCK_REALTIME, &end)==0);
-    double duration = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_nsec-start.tv_nsec)/1000000.0;
-
-    printf("Duration: %2f\n", duration);
-
-    mkl_free(matA);
-    mkl_free(matB);
-    mkl_free(matC);
-
+    printf("----------row partition----------------\n");
+    row_partition(n);
+    printf("----------column partition-------------\n");
+    column_partition(n);
     return 0;
 }
