10844cc08a583a93566e3e6cde06c452c1426466
jinyang <jinyang@cs.nyu.edu>
2017-03-28 18:25:21 +0000

    python3

--- nptest.py
+++ nptest.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import numpy as np
 import sys
 import timeit
 
 def main():
     n = 1000
-    print len(sys.argv)
     if len(sys.argv) > 1:
-        print sys.argv[1]
         n = int(sys.argv[1])
+    print("Matrix size %d" % n)
     matA = np.random.rand(n,n) 
     matB = np.random.rand(n,n) 
     dur = timeit.timeit(lambda: np.dot(matA, matB), number=1)
-    print dur
+    print(dur)
 
 if __name__ =='__main__':
     main()
